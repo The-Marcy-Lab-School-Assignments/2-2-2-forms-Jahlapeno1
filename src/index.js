c4b4b5f2b76df4ba909add7993ffcc68d9254262
Jahlapeno1 <jimenezaot@gmail.com>
2024-06-04 16:10:31 -0400

    FINISHED

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
  *
  */
 
-const handleSubmit = () => {
+
+
+const handleSubmit = (e) => {
+  const form = e.target
+
+  document.querySelector('#results-username').textContent = form.username.value
+  document.querySelector('#results-coding-level').textContent = form.codingLevel.value
+  document.querySelector('#results-location').textContent = form.location.value
+  if (form.didLikeAssignment.checked) {
+    document.querySelector('#results-did-like-assignment').textContent = "Yes"
+  } else {
+    document.querySelector('#results-did-like-assignment').textContent = "No"
+  }
+
+  form.reset()
+  // console.log(e)
+  e.preventDefault()
 };
+
+const main = () => {
+  // console.log(document.querySelector('form'))
+  document.querySelector('form').addEventListener('submit', handleSubmit)
+}
+
+main()
